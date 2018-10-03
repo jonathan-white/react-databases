d64c39ee7e5a1842bdc3d849858d7a72ae3006e0
Jon White <jon.white2@gmail.com>
2018-10-03 07:30:42 -0400

    formatting updates

--- client/src/Components/ModalForms/ModalForms.js
+++ client/src/Components/ModalForms/ModalForms.js
@@ -70,8 +70,8 @@ class ModalForms extends React.Component {
 			type: dbType || 'MySQL'
 		};
 		const isValidDB = dbTitle !== null && dbTitle !== '' &&
-		dbSummary !== null && dbSummary !== '' &&
-		dbType !== null && dbType !== '';
+			dbSummary !== null && dbSummary !== '' &&
+			dbType !== null && dbType !== '';
 		
 		// Table Form Data
 		const tableData = {
@@ -104,225 +104,226 @@ class ModalForms extends React.Component {
 			summary: prjSummary,
 			website: prjWebsite
 		};
-		const isValidProject = prjTitle !== null && prjTitle !== '' &&
-		prjSummary !== null && prjSummary !== '';
+		const isValidProject = prjTitle !== null && prjTitle !== '' && 
+			prjSummary !== null && prjSummary !== '';
 
 		return(
-		<div>
-			{/* New Database Modal Form */}
-			<Modal isOpen={showDBModal} toggle={() => actions.toggleModal('showDBModal')}>
-				<Form>
-					<ModalHeader toggle={() => this.props.toggleModal('showDBModal')}>
-						Add Database
-					</ModalHeader>
-					<ModalBody>
-						<FormGroup>
-							<Label for="dbTitle">
-								Database Name <span className="required">*</span>
-							</Label>
-							<Input type="text" name="dbTitle" id="dbTitle" autoFocus
-								onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
-						</FormGroup>
-						<FormGroup>
-							<Label for="dbSummary">
-								Description <span className="required">*</span>
-							</Label>
-							<Input type="textarea" name="dbSummary" id="dbSummary"
-								onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}/>
-						</FormGroup>
-						<FormGroup>
-							<Label for="dbType">
-								Type <span className="required">*</span>
-							</Label>
-							<Input type="select" name="dbType" id="dbType"
-								onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}>
-								<option>MySQL</option>
-								<option>MongoDB</option>
-							</Input>
-						</FormGroup>
-					</ModalBody>
-					<ModalFooter>
-						<FormText color="muted">
-							<span className="required">*</span> = Required Field
-						</FormText>
-						<Button color={`${isValidDB ? 'primary' : 'secondary'}`}
-							disabled={!isValidDB} onClick={(e) => {
-								this.props.toggleModal('showDBModal');
-								this.props.addDatabase(e, dbData);
-							}}>Save</Button>{' '}
-						<Button color="danger" onClick={() => this.props.toggleModal('showDBModal')}>
-							Cancel
-						</Button>
-					</ModalFooter>
-				</Form>
-			</Modal>
+			<div>
+				{/* New Database Modal Form */}
+				<Modal isOpen={showDBModal} toggle={() => actions.toggleModal('showDBModal')}>
+					<Form>
+						<ModalHeader toggle={() => this.props.toggleModal('showDBModal')}>
+							Add Database
+						</ModalHeader>
+						<ModalBody>
+							<FormGroup>
+								<Label for="dbTitle">
+									Database Name <span className="required">*</span>
+								</Label>
+								<Input type="text" name="dbTitle" id="dbTitle" autoFocus
+									onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
+							</FormGroup>
+							<FormGroup>
+								<Label for="dbSummary">
+									Description <span className="required">*</span>
+								</Label>
+								<Input type="textarea" name="dbSummary" id="dbSummary"
+									onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}/>
+							</FormGroup>
+							<FormGroup>
+								<Label for="dbType">
+									Type <span className="required">*</span>
+								</Label>
+								<Input type="select" name="dbType" id="dbType"
+									onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}>
+									<option>MySQL</option>
+									<option>MongoDB</option>
+								</Input>
+							</FormGroup>
+						</ModalBody>
+						<ModalFooter>
+							<FormText color="muted">
+								<span className="required">*</span> = Required Field
+							</FormText>
+							<Button color={`${isValidDB ? 'primary' : 'secondary'}`}
+								disabled={!isValidDB} onClick={(e) => {
+									this.props.toggleModal('showDBModal');
+									this.props.addDatabase(e, dbData);
+								}}>Save</Button>{' '}
+							<Button color="danger" onClick={() => this.props.toggleModal('showDBModal')}>
+								Cancel
+							</Button>
+						</ModalFooter>
+					</Form>
+				</Modal>
 
-			{/* Table Modal */}
-			<Modal isOpen={showTableModal} toggle={() => this.props.toggleModal('showTableModal')}>
-				<Form>
-					<ModalHeader toggle={() => this.props.toggleModal('showTableModal')}>
-						Add New Table
-					</ModalHeader>
-					<ModalBody>
-						<FormGroup>
-							<Label for="tbTitle">
-								Table Name <span className="required">*</span>
-							</Label>
-							<Input type="text" name="tbTitle" id="tbTitle" autoFocus
-								onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
-						</FormGroup>
-						<FormGroup>
-							<Label for="tbSummary">Description</Label>
-							<Input type="textarea" name="tbSummary" id="tbSummary"
-								onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}/>
-						</FormGroup>
-						<FormGroup>
-							<Label for="tbRecordsCount">Number of Records</Label>
-							<Input type="number" name="tbRecordCount" id="tbRecordCount"
-								onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
-						</FormGroup>
-					</ModalBody>
-					<ModalFooter>
-						<FormText color="muted">
-							<span className="required">*</span> = Required Field
-						</FormText>
-						<Button color={`${isValidTable ? 'primary' : 'secondary'}`}
-							disabled={!isValidTable} onClick={(e) => {
-								this.props.toggleModal('showTableModal');
-								this.props.addTable(e, tableData);
-							}}>Save</Button>{' '}
-						<Button color="danger" onClick={() => this.props.toggleModal('showTableModal')}>
-							Cancel
-						</Button>
-					</ModalFooter>
-				</Form>
-			</Modal>
+				{/* Table Modal */}
+				<Modal isOpen={showTableModal} toggle={() => this.props.toggleModal('showTableModal')}>
+					<Form>
+						<ModalHeader toggle={() => this.props.toggleModal('showTableModal')}>
+							Add New Table
+						</ModalHeader>
+						<ModalBody>
+							<FormGroup>
+								<Label for="tbTitle">
+									Table Name <span className="required">*</span>
+								</Label>
+								<Input type="text" name="tbTitle" id="tbTitle" autoFocus
+									onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
+							</FormGroup>
+							<FormGroup>
+								<Label for="tbSummary">Description</Label>
+								<Input type="textarea" name="tbSummary" id="tbSummary"
+									onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}/>
+							</FormGroup>
+							<FormGroup>
+								<Label for="tbRecordsCount">Number of Records</Label>
+								<Input type="number" name="tbRecordCount" id="tbRecordCount"
+									onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
+							</FormGroup>
+						</ModalBody>
+						<ModalFooter>
+							<FormText color="muted">
+								<span className="required">*</span> = Required Field
+							</FormText>
+							<Button color={`${isValidTable ? 'primary' : 'secondary'}`}
+								disabled={!isValidTable} onClick={(e) => {
+									this.props.toggleModal('showTableModal');
+									this.props.addTable(e, tableData);
+								}}>Save</Button>{' '}
+							<Button color="danger" onClick={() => this.props.toggleModal('showTableModal')}>
+								Cancel
+							</Button>
+						</ModalFooter>
+					</Form>
+				</Modal>
 
-			{/* Field Modal */}
-			<Modal isOpen={showFieldModal} toggle={() => this.props.toggleModal('showFieldModal')}>
-				<Form>
-					<ModalHeader toggle={() => this.props.toggleModal('showFieldModal')}>
-						New Field Details
-					</ModalHeader>
-					<ModalBody>
-						<FormGroup>
-							<Label for="fdTitle">
-								Field Name <span className="required">*</span>
-							</Label>
-							<Input type="text" name="fdTitle" id="fdTitle"
-								onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}/>
-						</FormGroup>
-						<FormGroup>
-							<Label for="fdSummary">Description</Label>
-							<Input type="textarea" name="fdSummary" id="fdSummary"
-								onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}/>
-						</FormGroup>
-						<div className="d-flex">
-							<div className="mr-2">
-								<FormGroup>
-									<Label for="fdDataType">Data Type</Label>
-									<Input type="select" name="fdDataType" id="fdDataType"
-										defaultValue="varchar"
-										onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}>
-										<option>boolean</option>
-										<option>datetime</option>
-										<option>decimal</option>
-										<option>double</option>
-										<option>int</option>
-										<option>text</option>
-										<option>tinyint</option>
-										<option>varchar</option>
-									</Input>
-								</FormGroup>
-								<FormGroup>
-									<Label for="fdDataLength">Length</Label>
-									<Input type="number" name="fdDataLength" id="fdDataLength"
-										onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
-								</FormGroup>
+				{/* Field Modal */}
+				<Modal isOpen={showFieldModal} toggle={() => this.props.toggleModal('showFieldModal')}>
+					<Form>
+						<ModalHeader toggle={() => this.props.toggleModal('showFieldModal')}>
+							New Field Details
+						</ModalHeader>
+						<ModalBody>
+							<FormGroup>
+								<Label for="fdTitle">
+									Field Name <span className="required">*</span>
+								</Label>
+								<Input type="text" name="fdTitle" id="fdTitle"
+									onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}/>
+							</FormGroup>
+							<FormGroup>
+								<Label for="fdSummary">Description</Label>
+								<Input type="textarea" name="fdSummary" id="fdSummary"
+									onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}/>
+							</FormGroup>
+							<div className="d-flex">
+								<div className="mr-2">
+									<FormGroup>
+										<Label for="fdDataType">Data Type</Label>
+										<Input type="select" name="fdDataType" id="fdDataType"
+											defaultValue="varchar"
+											onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}>
+											<option>boolean</option>
+											<option>datetime</option>
+											<option>decimal</option>
+											<option>double</option>
+											<option>int</option>
+											<option>text</option>
+											<option>tinyint</option>
+											<option>varchar</option>
+										</Input>
+									</FormGroup>
+									<FormGroup>
+										<Label for="fdDataLength">Length</Label>
+										<Input type="number" name="fdDataLength" id="fdDataLength"
+											onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
+									</FormGroup>
+								</div>
+								<div className="ml-2">
+									<FormGroup>
+										<Label for="fdDefaultValue">Default Value</Label>
+										<Input type="text" name="fdDefaultValue" id="fdDefaultValue"
+											onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
+									</FormGroup>
+									<FormGroup>
+										<Label for="fdKey">Key</Label>
+										<Input type="text" name="fdKey" id="fdKey"
+											onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
+									</FormGroup>
+									<FormGroup check>
+										<Label check for="fdAllowNull">
+											<Input type="checkbox" name="fdAllowNull" id="fdAllowNull"
+												onChange={(e) => this.props.handleInputChange(e.target.name, e.target.checked)} />
+											Allow Nulls
+										</Label>
+									</FormGroup>
+								</div>
 							</div>
-							<div className="ml-2">
-								<FormGroup>
-									<Label for="fdDefaultValue">Default Value</Label>
-									<Input type="text" name="fdDefaultValue" id="fdDefaultValue"
-										onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
-								</FormGroup>
-								<FormGroup>
-									<Label for="fdKey">Key</Label>
-									<Input type="text" name="fdKey" id="fdKey"
-										onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
-								</FormGroup>
-								<FormGroup check>
-									<Label check for="fdAllowNull">
-										<Input type="checkbox" name="fdAllowNull" id="fdAllowNull"
-											onChange={(e) => this.props.handleInputChange(e.target.name, e.target.checked)} />
-										Allow Nulls
-									</Label>
-								</FormGroup>
-							</div>
-						</div>
-					</ModalBody>
-					<ModalFooter>
-						<FormText color="muted">
-							<span className="required">*</span> = Required Field
-						</FormText>
-						<Button color={`${isValidField ? 'primary' : 'secondary'}`}
-							disabled={!isValidField} onClick={(e) => {
-								this.props.toggleModal('showFieldModal');
-								this.props.addField(e, fieldData);
-							}}>Save</Button>{' '}
-						<Button color="danger" onClick={() => this.props.toggleModal('showFieldModal')}>
-							Cancel
-						</Button>
-					</ModalFooter>
-				</Form>
-			</Modal>
+						</ModalBody>
+						<ModalFooter>
+							<FormText color="muted">
+								<span className="required">*</span> = Required Field
+							</FormText>
+							<Button color={`${isValidField ? 'primary' : 'secondary'}`}
+								disabled={!isValidField} onClick={(e) => {
+									this.props.toggleModal('showFieldModal');
+									this.props.addField(e, fieldData);
+								}}>Save</Button>{' '}
+							<Button color="danger" onClick={() => this.props.toggleModal('showFieldModal')}>
+								Cancel
+							</Button>
+						</ModalFooter>
+					</Form>
+				</Modal>
 
-			{/* Project Modal */}
-			<Modal isOpen={showProjectModal} toggle={() => this.props.toggleModal('showProjectModal')}>
-				<Form>
-					<ModalHeader toggle={() => this.props.toggleModal('showProjectModal')}>
-						Add Project
-					</ModalHeader>
-					<ModalBody>
-						<FormGroup>
-							<Label for="prjTitle">
-								Project Name <span className="required">*</span>
-							</Label>
-							<Input type="text" name="prjTitle" id="prjTitle"
-								onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
-						</FormGroup>
-						<FormGroup>
-							<Label for="prjSummary">
-								Description <span className="required">*</span>
-							</Label>
-							<Input type="textarea" name="prjSummary" id="prjSummary"
-								onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}/>
-						</FormGroup>
-						<FormGroup>
-							<Label for="prjWebsite">
-								Website
-							</Label>
-							<Input type="url" name="prjWebsite" id="prjWebsite"
-								onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
-						</FormGroup>
-					</ModalBody>
-					<ModalFooter>
-						<FormText color="muted">
-							<span className="required">*</span> = Required Field
-						</FormText>
-						<Button color={`${isValidProject ? 'primary' : 'secondary'}`}
-							disabled={!isValidProject}
-							onClick={(e) => {
-								this.props.addProjectToDB(e, selectedDB._id, projectData);
-								this.props.toggleModal('showProjectModal');
-							}}>Save</Button>{' '}
-						<Button color="danger"
-							onClick={() => this.props.toggleModal('showProjectModal')}>Cancel</Button>
-					</ModalFooter>
-				</Form>
-			</Modal>
-		</div>
-	)}
+				{/* Project Modal */}
+				<Modal isOpen={showProjectModal} toggle={() => this.props.toggleModal('showProjectModal')}>
+					<Form>
+						<ModalHeader toggle={() => this.props.toggleModal('showProjectModal')}>
+							Add Project
+						</ModalHeader>
+						<ModalBody>
+							<FormGroup>
+								<Label for="prjTitle">
+									Project Name <span className="required">*</span>
+								</Label>
+								<Input type="text" name="prjTitle" id="prjTitle"
+									onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
+							</FormGroup>
+							<FormGroup>
+								<Label for="prjSummary">
+									Description <span className="required">*</span>
+								</Label>
+								<Input type="textarea" name="prjSummary" id="prjSummary"
+									onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)}/>
+							</FormGroup>
+							<FormGroup>
+								<Label for="prjWebsite">
+									Website
+								</Label>
+								<Input type="url" name="prjWebsite" id="prjWebsite"
+									onChange={(e) => this.props.handleInputChange(e.target.name, e.target.value)} />
+							</FormGroup>
+						</ModalBody>
+						<ModalFooter>
+							<FormText color="muted">
+								<span className="required">*</span> = Required Field
+							</FormText>
+							<Button color={`${isValidProject ? 'primary' : 'secondary'}`}
+								disabled={!isValidProject}
+								onClick={(e) => {
+									this.props.addProjectToDB(e, selectedDB._id, projectData);
+									this.props.toggleModal('showProjectModal');
+								}}>Save</Button>{' '}
+							<Button color="danger"
+								onClick={() => this.props.toggleModal('showProjectModal')}>Cancel</Button>
+						</ModalFooter>
+					</Form>
+				</Modal>
+			</div>
+		)
+	}
 };
 
 const Modals = connect(
